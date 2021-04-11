bf93ff23f0a0dcccd01d7440e5d14a131476bad7
Bryan Kwong <bfkwong@calpoly.edu>
2021-04-11 14:58:00 -0700

    chore: configure amplify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
-  return <div>Eventing</div>;
+  return (
+    <div>
+      Eventing
+      <AmplifySignOut />
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App);
